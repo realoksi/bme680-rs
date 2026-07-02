9b1856862d7a3b01189221610eda560a0da383d9
realoksi <realoksi@pm.me>
2026-07-02 00:50:15 -0400

    included memory map module and created baseline BME680 impl

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,8 @@
 #![allow(non_camel_case_types)]
 #![allow(dead_code)]
 
+mod memory_map;
+
 pub type Result<T> = core::result::Result<T, ()>;
 
 pub trait Layer {
@@ -9,13 +11,24 @@ pub trait Layer {
     fn write_byte(&mut self, addr: u8, data: u8) -> Result<()>;
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub struct BME680<L>
+where
+    L: Layer,
+{
+    transport_layer: L,
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl<L> BME680<L>
+where
+    L: Layer,
+{
+    pub fn new(transport_layer: L) -> Self {
+        Self {
+            transport_layer
+        }
+    }
+
+    pub fn get_chip_id(&mut self) -> u8 {
+        self.transport_layer.read_byte(memory_map::chip_id::get_addr()).unwrap()
     }
 }
