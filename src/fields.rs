a7bc079578b9506a24b6088ffca99d06a4db601b
realoksi <realoksi@pm.me>
2026-07-06 09:36:35 -0400

    applied to derive From and Into for bitfields and added CtrlMeas

--- src/fields.rs
+++ src/fields.rs
@@ -1,39 +1,26 @@
 use bitfield::bitfield;
+use derive_more::{From, Into};
 
 bitfield! {
+    #[derive(Debug, From, Into)]
     pub struct Status(u8);
 
     pub get_spi_mem_page, set_spi_mem_page: 4, 4;
 }
 
-impl From<u8> for Status {
-    fn from(value: u8) -> Self {
-        Status(value)
-    }
-}
-
-impl Into<u8> for Status {
-    fn into(self) -> u8 {
-        self.0
-    }
-}
-
 bitfield! {
+    #[derive(Debug, From, Into)]
     pub struct Config(u8);
 
     pub get_filter, set_filter: 4, 2;
     pub get_spi_3w_en, set_spi_3w_en: 0, 0;
 }
 
-impl From<u8> for Config {
-    fn from(value: u8) -> Self {
-        Config(value)
-    }
-}
+bitfield! {
+    #[derive(Debug, From, Into)]
+    pub struct CtrlMeas(u8);
 
-impl Into<u8> for Config {
-    fn into(self) -> u8 {
-        self.0
-    }
+    pub get_osrs_t, set_osrs_t: 7, 5;
+    pub get_osrs_p, set_osrs_p: 4, 2;
+    pub get_mode, set_mode: 1, 0;
 }
-
