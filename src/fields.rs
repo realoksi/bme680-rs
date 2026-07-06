484231e672d24924e22d173a5827a7413ca78d19
realoksi <realoksi@pm.me>
2026-07-06 14:17:16 -0400

    added more bitfields

--- src/fields.rs
+++ src/fields.rs
@@ -24,3 +24,60 @@ bitfield! {
     pub get_osrs_p, set_osrs_p: 4, 2;
     pub get_mode, set_mode: 1, 0;
 }
+
+bitfield! {
+    #[derive(Debug, From, Into)]
+    pub struct CtrlHum(u8);
+
+    pub get_spi_3w_int_en, set_spi_3w_int_en: 6, 6;
+    pub get_osrs_h, set_osrs_h: 2, 0;
+}
+
+bitfield! {
+    #[derive(Debug, From, Into)]
+    pub struct CtrlGas1(u8);
+
+    pub get_run_gas, set_run_gas: 5, 5;
+    pub get_nb_conv, set_nb_conv: 3, 0;
+}
+
+bitfield! {
+    #[derive(Debug, From, Into)]
+    pub struct CtrlGas0(u8);
+
+    pub get_heat_off, set_heat_off: 3, 3;
+}
+
+bitfield! {
+    #[derive(Debug, From, Into)]
+    pub struct GasRLsb2(u8);
+
+    pub get_gas_r, _: 6, 7;
+    pub get_gas_valid_r, _: 5, 5;
+    pub get_heat_stab_r, _: 4, 4;
+    pub get_gas_range_r, _: 0, 3;
+}
+
+bitfield! {
+    #[derive(Debug, From, Into)]
+    pub struct TempXlsb2(u8);
+
+    pub get_temp_xlsb, _: 7, 4;
+}
+
+bitfield! {
+    #[derive(Debug, From, Into)]
+    pub struct PressXlsb2(u8);
+
+    pub get_press_xlsb, _: 7, 4;
+}
+
+bitfield! {
+    #[derive(Debug, From, Into)]
+    pub struct MeasStatus2(u8);
+
+    pub get_new_data, _: 7, 7;
+    pub get_gas_measuring, _: 6, 6;
+    pub get_measuring, _: 5, 5;
+    pub get_gas_meas_index_2, _: 3, 0;
+}
