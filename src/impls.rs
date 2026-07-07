96195c199230320ef9cd43be6a367a535fd1212b
realoksi <realoksi@pm.me>
2026-07-06 22:53:10 -0400

    implemented `I2c<File>` rw methods

--- src/impls.rs
+++ src/impls.rs
@@ -1,14 +1,11 @@
 use crate::Layer;
 
-#[cfg(feature = "std")]
-use std::fs::File;
-
 #[cfg(feature = "i2c-linux")]
-impl<I> Layer for i2c_linux::I2c<I> {
+impl Layer for i2c_linux::I2c<std::fs::File> {
     fn read_byte(&mut self, addr: u8) -> crate::Result<u8> {
-        todo!()
+        self.smbus_read_byte_data(addr).map_err(|_| ())
     }
     fn write_byte(&mut self, addr: u8, data: u8) -> crate::Result<()> {
-        todo!()
+        self.smbus_write_byte_data(addr, data).map_err(|_| ())
     }
 }
